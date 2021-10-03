ded6d919caad25ad2d9b9b62e80fc779c07b18ca
Martin Johansen <Martin.Johansen@no.experis.com>
2021-10-03 16:22:45 +0200

    Fixed enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 //Environment for production
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl : 'https://vuejs-assignment-3.herokuapp.com/trainers',
+  apiKey : 'RakrKsq94tlf3PZmAnuQ',
+  apiPokemon: "https://pokeapi.co/api/v2/pokemon/"
 };
